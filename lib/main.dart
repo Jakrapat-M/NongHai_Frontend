70694b291611e0804d0ed40e67aaf4d9ea778ec5
Mr.JAKRAPAT MATURES <64130500209@st.sit.kmutt.ac.th>
2024-08-22 13:54:38 +0700

    revert

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:nonghai/firebase_options.dart';
 import 'package:nonghai/pages/login_page.dart';
 import 'package:nonghai/pages/register_page.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main() {
   runApp(const MyApp());
 }
 
