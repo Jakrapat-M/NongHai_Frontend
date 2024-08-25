05f19fa8917fa9194d52b3e0efd9f15457164642
Kevalin Srisakulpong <91175693+Ryyyyyy12@users.noreply.github.com>
2024-08-26 00:43:55 +0700

    [ADD] tracking page

---
+++ lib/pages/tracking_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:nonghai/components/custom_appbar.dart';
+
+class TrackingPage extends StatefulWidget {
+  const TrackingPage({Key? key}) : super(key: key);
+
+  @override
+  State<TrackingPage> createState() => _TrackingPageState();
+}
+
+class _TrackingPageState extends State<TrackingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      appBar: CustomAppBar(title: 'Tracking'),
+      body: const SafeArea(
+        child: Center(
+          child: Column(
+            children:[
+              
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
