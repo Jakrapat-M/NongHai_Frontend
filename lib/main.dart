23568334a9ba22c31e065ba97265b5077c091822
Mr.JAKRAPAT MATURES <jakrapat.matu@kmutt.ac.th>
2024-10-01 16:37:42 +0700

    add Background Handler?

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
-import 'dart:async';
-import 'package:app_links/app_links.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 // import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:nonghai/pages/nfc_page.dart';
 // import 'package:flutter/painting.dart';
 // import 'package:flutter/rendering.dart';
 // import 'package:flutter/widgets.dart';
-import 'package:nonghai/pages/test_nfc_page.dart';
+import 'package:nonghai/pages/nfc_page.dart';
 import 'package:nonghai/pages/tracking_page.dart';
 import 'package:nonghai/services/auth/login_or_registoer.dart';
 import 'package:nonghai/firebase_options.dart';
@@ -17,89 +15,38 @@ import 'package:nonghai/services/auth/auth_gate.dart';
 import 'package:nonghai/services/noti/noti_service.dart';
 // import 'package:nonghai/services/auth/auth_service.dart';
 // import 'package:provider/provider.dart';
-import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  await NotiService().init();
-  await initializeDateFormatting('th_TH', null);
+
+  await NotificationService().initialize();
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final _navigatorKey = GlobalKey<NavigatorState>();
-  late AppLinks _appLinks;
-  StreamSubscription<Uri>? _linkSubscription;
-
-  @override
-  void initState() {
-    super.initState();
-    initDeepLinks();
-  }
-
-  @override
-  void dispose() {
-    _linkSubscription?.cancel();
-    super.dispose();
-  }
-
-  Future<void> initDeepLinks() async {
-    _appLinks = AppLinks();
-
-    // Handle links
-    _linkSubscription = _appLinks.uriLinkStream.listen((uri) {
-      debugPrint('onAppLink: $uri');
-      openAppLink(uri);
-    });
-  }
-
-  void openAppLink(Uri uri) {
-    final fragment = uri.fragment;
-
-    // Print the full URI for debugging purposes
-    debugPrint('Navigating to: $fragment');
-
-    // Navigate to TrackingPage
-    _navigatorKey.currentState?.push(MaterialPageRoute(
-      builder: (context) {
-        return TrackingPage(
-          petId: fragment,
-          petName: 'Ella',
-          petImage: 'petImage',
-        );
-      },
-    ));
-  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      navigatorKey: _navigatorKey,
       title: 'Nonghai',
       theme: ThemeData(
         colorScheme: ThemeData().colorScheme.copyWith(
-            primary: const Color(0xffC8A48A),
-            secondary: const Color(0xffE8E8E8),
-            surface: const Color(0xffF2F2F2), // page background
-            tertiary: const Color(0xffFfffff), //textfield
-            onSurface: const Color(0xff2C3F50), //blue surface(box/button)
-            secondaryContainer: const Color(0xffE8E8E8), //container
-            secondaryFixed: const Color(0xff2C3F50), //container
-            surfaceBright:
-                const Color(0xff5DB671), // green container box(status)
-            onErrorContainer: Colors.red // red container box(status)
+              primary: const Color(0xffC8A48A),
+              secondary: const Color(0xffE8E8E8),
+              surface: const Color(0xffF2F2F2), // background
+              tertiary: const Color(0xffFfffff), //textfield
+              secondaryContainer: const Color(0xffE8E8E8), //container
+              secondaryFixed: const Color(0xff2C3F50), //container
             ),
         useMaterial3: true,
         textTheme: const TextTheme(
@@ -118,36 +65,11 @@ class _MyAppState extends State<MyApp> {
               fontWeight: FontWeight.w400,
               color: Color(0xff2C3F50),
               fontFamily: 'Fredoka'),
-          labelMedium: TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.w400,
-              color: Color(0xffC8A48A),
-              fontFamily: 'Fredoka'),
-          labelSmall: TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.w400,
-              color: Color(0xffFfffff),
-              fontFamily: 'Fredoka'),
           bodySmall: TextStyle(
               fontSize: 14,
               fontWeight: FontWeight.w400,
               color: Color(0xff8C8C8C),
               fontFamily: 'Fredoka'),
-          labelLarge: TextStyle(
-              fontFamily: 'Fredoka',
-              fontSize: 11,
-              fontWeight: FontWeight.w600,
-              color: Color(0xff1E1E1E)),
-          // labelMedium: TextStyle(
-          //     fontFamily: 'Fredoka',
-          //     fontSize: 9,
-          //     fontWeight: FontWeight.w500,
-          //     color: Color(0xff5C5C5C)),
-          // labelSmall: TextStyle(
-          //     fontFamily: 'Fredoka',
-          //     fontSize: 8,
-          //     fontWeight: FontWeight.w600,
-          //     color: Color(0xffffffff)),
         ),
         appBarTheme: const AppBarTheme(
           titleTextStyle: TextStyle(
@@ -155,26 +77,28 @@ class _MyAppState extends State<MyApp> {
               fontWeight: FontWeight.w500,
               color: Color(0xff2C3F50),
               fontFamily: 'Fredoka'),
-          backgroundColor: Color(0xffffffff),
+          backgroundColor: Colors.transparent,
           elevation: 0,
           centerTitle: true,
-          iconTheme: IconThemeData(color: Color(0xff2C3F50), size: 25),
+          iconTheme: IconThemeData(color: Color(0xff2C3F50), size: 36),
         ),
       ),
-      initialRoute: '/nfc',
+      initialRoute: '/',
       routes: {
         '/': (context) => const AuthGate(),
         '/loginOrRegister': (context) => const LoginOrRegistoer(),
         '/home': (context) => const HomePage(),
-        '/testnfc': (context) => const TestNfcPage(),
-        '/nfc': (context) => const NfcPage(
-              petId: '550e8400-e29b-41d4-a716-446655440000',
-            ),
+        '/nfc': (context) => const NfcPage(),
         '/tracking': (context) => const TrackingPage(
-            petId: '550e8400-e29b-41d4-a716-446655440000',
+            petId: '318f9090-1613-4016-8d16-0f2de8223564',
             petName: 'Ella',
             petImage: 'assets/images/test.jpg'),
       },
     );
   }
+}
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('Handling a background message ${message.messageId}');
 }
--- lib/services/noti/noti_service.dart
+++ lib/services/noti/noti_service.dart
@@ -1,22 +1,73 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 
-class NotiService {
-  final _firebaseMessaging = FirebaseMessaging.instance;
+import 'package:flutter/material.dart';
 
-  Future<void> init() async {
-    await _firebaseMessaging.requestPermission();
-    final fCMToken = await _firebaseMessaging.getToken();
+// class NotiService {
+//   final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+//   final _firebaseMessaging = FirebaseMessaging.instance;
 
-    if (kDebugMode) {
-      print('FCM Token: $fCMToken');
+//   Future<void> init() async {
+//     final notificationSetion = await _firebaseMessaging.requestPermission(provisional: true);
+
+//     final apnsToken = await _firebaseMessaging.getAPNSToken();
+//     if (apnsToken != null) {
+//       print('APNS Token: $apnsToken');
+//       // make FCM plugin API request
+//     }
+
+//     final fCMToken = await _firebaseMessaging.getToken();
+
+//     if (kDebugMode) {
+//       print('FCM Token: $fCMToken');
+//     }
+//   }
+
+//   // handle message
+//   void handleMessage(RemoteMessage? message) {
+//     if (message == null) return;
+// // navigate to page
+//     navigatorKey.currentState?.pushNamed('/notification', arguments: message);
+//   }
+
+//   Future initPushNotification() async {
+//     // handle notification when app is terminated and now opened
+//     FirebaseMessaging.instance.getInitialMessage().then(handleMessage);
+
+//     FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
+//   }
+// }
+
+class NotificationService {
+  final FirebaseMessaging _messaging = FirebaseMessaging.instance;
+
+  Future<void> initialize() async {
+    // Request permissions for iOS
+    NotificationSettings settings = await _messaging.requestPermission(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('User granted permission');
+    } else {
+      print('User declined or has not accepted permission');
     }
 
-    // handle message
-    void handleMessage(RemoteMessage? message) {
-      if (message != null) return;
+    // Get the device token
+    String? token = await _messaging.getToken();
+    print("Device Token: $token");
 
-      // navigate to page
-    }
+    // Handle foreground messages
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Received a message in the foreground: ${message.notification?.title}');
+      _showNotification(message);
+    });
+  }
+
+  // Show a notification
+  void _showNotification(RemoteMessage message) {
+    // Use a package like flutter_local_notifications to show a notification
   }
 }
